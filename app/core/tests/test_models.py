cab600ef2edb48746a08fabc24f5fcd7e930db05
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 <vtcampos90@gmail.com>
2023-10-08 20:35:50 -0300

    Build user API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
-class ModelTest(TestCase):
+class ModelTests(TestCase):
     """Test Models."""
 
     def test_create_user_with_email_succesful(self):
--- app/user/views.py
+++ app/user/views.py
@@ -22,7 +22,7 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManageUserView(generics.RetrieveAPIView):
+class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user."""
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
