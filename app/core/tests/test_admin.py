cb330c602567f19452967b828b6989388502c0e0
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 <vtcampos90@gmail.com>
2023-10-08 20:23:25 -0300

    Build user API

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -21,10 +21,10 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
-            name='Test User',
+            name='Test User'
         )
 
-    def test_users_list(self):
+    def test_users_lists(self):
         """Test that users are listed on page."""
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -25,7 +25,7 @@ class ModelTest(TestCase):
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
@@ -41,7 +41,7 @@ class ModelTest(TestCase):
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
-            'test123'
+            'test123',
         )
 
         self.assertTrue(user.is_superuser)
